5b533bc59545a4e83a30d7970d3c4d53abfe9510
borutoboy <96014170+muuhamed7limem@users.noreply.github.com>
2023-03-30 03:44:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector("#qr-code");
+const qrText = document.querySelector(".qr-text");
+const sizes = document.querySelector(".sizes");
+
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change", handleSize);
+
+const defaultUrl = "https://youtube.com/@AsmrProg";
+let colorLight = "#fff",
+    colorDark = "#000",
+    text = defaultUrl,
+    size = 300;
+
+function handleDarkColor(e) {
+    colorDark = e.target.value;
+    generateQRCode();
+}
+
+function handleLightColor(e) {
+    colorLight = e.target.value;
+    generateQRCode();
+}
+
+function handleQRText(e) {
+    const value = e.target.value;
+    text = value;
+    if (!value) {
+        text = defaultUrl;
+    }
+    generateQRCode();
+}
+
+async function generateQRCode() {
+    qrContainer.innerHTML = "";
+    new QRCode("qr-code", {
+        text,
+        height: size,
+        width: size,
+        colorLight,
+        colorDark,
+    });
+    download.href = await resolveDataUrl();
+}
+
+
+function handleSize(e) {
+    size = e.target.value;
+    generateQRCode();
+}
+
+function resolveDataUrl() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = document.querySelector("#qr-code img");
+            if (img.currentSrc) {
+                resolve(img.currentSrc);
+                return;
+            }
+            const canvas = document.querySelector("canvas");
+            resolve(canvas.toDataURL());
+        }, 50);
+    });
+}
+generateQRCode();
